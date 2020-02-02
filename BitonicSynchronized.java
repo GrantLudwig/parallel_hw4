4b6377134e06274c5a1a436e9424a3625bc512dd
unknown <grantludwig97@gmail.com>
2020-02-01 23:42:43 -0800

    16 threads

--- BitonicSynchronized.java
+++ BitonicSynchronized.java
@@ -12,10 +12,9 @@ import java.lang.Math;
 
 public class BitonicSynchronized {
     public static final int N = 1 << 22;  // size of the final sorted array (power of two)
-    //public static final int N = 16; // needs to be a multiple of 2
-    public static final int P = 8; // number of threads
-    //public static final int TIME_ALLOWED = 10;  // seconds
-    public static final int TIME_ALLOWED = 10;
+    public static final int P = 16; // number of threads
+    public static final int TIME_ALLOWED = 10;  // seconds
+
     public static double[] data;
 
     /**
