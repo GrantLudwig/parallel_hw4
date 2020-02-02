fdb17c7ebe6042f9a345ef9d85d4f9c0ae4e1e95
unknown <grantludwig97@gmail.com>
2020-02-01 23:29:15 -0800

    debugging

--- BitonicSynchronized.java
+++ BitonicSynchronized.java
@@ -11,8 +11,8 @@ import java.util.concurrent.BrokenBarrierException;
 import java.lang.Math;
 
 public class BitonicSynchronized {
-    public static final int N = 1 << 22;  // size of the final sorted array (power of two)
-    //public static final int N = 16; // needs to be a multiple of 2
+    //public static final int N = 1 << 22;  // size of the final sorted array (power of two)
+    public static final int N = 16; // needs to be a multiple of 2
     public static final int P = 8; // number of threads
     //public static final int TIME_ALLOWED = 10;  // seconds
     public static final int TIME_ALLOWED = 10;
